4a5dfe0b8270dbef65ce007c0bfbf3cf934772fc
Paulo Serpa Antunes <pauloserpaantunes@gmail.com>
2021-11-28 11:54:12 -0300

    first commit

---
+++ src/main/java/ifrs/pw2/paulo/GreetingConfig.java
@@ -0,0 +1,12 @@
+package ifrs.pw2.paulo;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/ifrs/pw2/paulo/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ifrs.pw2.paulo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
